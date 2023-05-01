87df7bd0e6f1dd50b7b844ae51c2c3840873e9cd
asib-hasan <asib.uucse@gmail.com>
2023-05-01 11:31:13 +0600

    first commit

---
+++ I_shop/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from store_app.models import Product,Categories
+
+def BASE(request):
+    return render(request,'Main/base.html')
+
+def HOME(request):
+    product = Product.objects.filter(status='Publish')
+    categories = Categories.objects.all()
+    context = {
+        'product': product,
+        'categories' : categories,
+    }
+    return render(request,'Main/index.html',context)
+
+
+def PRODUCT(request):
+    product = Product.objects.filter(status='Publish')
+
+
+    context = {
+        'product': product,
+    }
+    return render(request,'Main/product.html', context)
